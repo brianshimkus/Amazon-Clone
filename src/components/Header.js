e72cfa1f4f5c245d525978ec4c3f569ca45bea1e
Brian Shimkus <brianshimkus1@gmail.com>
2022-10-12 13:43:24 -0500

    Initial Heroicons install and setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 export default function Header() {
 	return (
 		<header>
 			{/* Top Nav */}
-			<div>
-				<div>
+			<div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+				<div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
 					<Image
 						src='https://links.papareact.com/f90'
 						width={150}
@@ -13,6 +14,11 @@ export default function Header() {
 						objectFit='contain'
 						className='cursor-pointer'
 					/>
+				</div>
+				{/* Search */}
+				<div className='bg-yellow-400'>
+					<input type='text' />
+					<MagnifyingGlassIcon className='h-12 p-4' />
 				</div>
 			</div>
 
