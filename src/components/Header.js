60bc3115b8889a622394f57c932d1e2a2261c617
Brian Shimkus <brianshimkus1@gmail.com>
2022-10-11 23:59:19 -0500

    Initial Header componenent setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,23 @@
+import Image from 'next/image'
+
 export default function Header() {
-	return <div>Header</div>
+	return (
+		<header>
+			{/* Top Nav */}
+			<div>
+				<div>
+					<Image
+						src='https://links.papareact.com/f90'
+						width={150}
+						height={40}
+						objectFit='contain'
+						className='cursor-pointer'
+					/>
+				</div>
+			</div>
+
+			{/* Bottom Nav */}
+			<div></div>
+		</header>
+	)
 }
