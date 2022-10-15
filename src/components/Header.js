0df5bcfaf78cf4949d9658121f5a834f8f6790c7
Brian Shimkus <brianshimkus1@gmail.com>
2022-10-14 23:08:29 -0500

    Search bar styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,8 +16,11 @@ export default function Header() {
 					/>
 				</div>
 				{/* Search */}
-				<div className='bg-yellow-400'>
-					<input type='text' />
+				<div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
+					<input
+						className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4'
+						type='text'
+					/>
 					<MagnifyingGlassIcon className='h-12 p-4' />
 				</div>
 			</div>
