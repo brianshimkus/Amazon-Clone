2d648c832edd846dc209ccd840984cc13d446841
Brian Shimkus <brianshimkus1@gmail.com>
2022-10-17 15:18:59 -0500

    Only show first four products

--- src/components/Product.js
+++ src/components/Product.js
@@ -19,8 +19,8 @@ export default function Product({
 	)
 	const [hasPrime] = useState(Math.random() < 0.5)
 	return (
-		<div className='relative flex flex-col m-5 bg-white z-30 p-10'>
-			<p className='absolute top-2 right-2 text-xs italic text-gray-400'>
+		<div className='relative flex flex-col m-5 bg-white z-30 p-10 rounded-md'>
+			<p className='absolute top-2 right-4 text-xs italic text-gray-400'>
 				{category}
 			</p>
 			<Image src={image} height={200} width={200} objectFit='contain' />
