4e4210c973e45451443b266dce7e5523478dbb4f
Brian Shimkus <brianshimkus1@gmail.com>
2022-10-18 13:05:23 -0500

    Built out left side of Checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Header from '../components/Header'
 
 export default function Checkout() {
@@ -5,7 +6,24 @@ export default function Checkout() {
 		<div className='bg-gray-100'>
 			<Header />
 
-			<h1>Checkout page</h1>
+			<main className='lg:flex max-w-screen-2xl mx-auto'>
+				{/* Left Section */}
+				<div className='flex-grow m-5 shadow-sm'>
+					<Image
+						src='https://links.papareact.com/ikj'
+						height={250}
+						width={1020}
+						objectFit='contain'
+					/>
+
+					<div className='flex flex-col p-5 space-y-10 bg-white'>
+						<h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
+					</div>
+				</div>
+
+				{/* Right Section */}
+				<div></div>
+			</main>
 		</div>
 	)
 }
