ffc686c47d225f6591a6c3d3d55e8783aabbaabc
Brian Shimkus <brianshimkus1@gmail.com>
2022-10-18 12:56:54 -0500

    Created checkout component

---
+++ src/pages/checkout.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+
+export default function Checkout() {
+	return (
+		<div className='bg-gray-100'>
+			<Header />
+
+			<h1>Checkout page</h1>
+		</div>
+	)
+}
