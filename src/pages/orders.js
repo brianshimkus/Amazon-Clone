ed7cf4e8dd6fb8c9e877b8ecb8cbbb2dc45ca2c9
Brian Shimkus <brianshimkus1@gmail.com>
2022-11-02 16:09:11 -0500

    Built out Orders page

---
+++ src/pages/orders.js
@@ -0,0 +1,77 @@
+import { useSession } from 'next-auth/react'
+import Header from '../components/Header'
+
+export default function OrdersPage({ orders }) {
+	const { data: session } = useSession()
+
+	return (
+		<div>
+			<Header />
+			<main className='max-w-screen-lg mx-auto p-10'>
+				<h1 className='text-3xl border-b mb-2 pb-1 border-yellow-400'>
+					Your Orders
+				</h1>
+
+				{session ? (
+					<h2>{orders.length} Orders</h2>
+				) : (
+					<h2>Please sign in to see your orders</h2>
+				)}
+
+				<div className='mt-5 space-y-4'>
+					{orders?.map(
+						({ id, amount, amount_shipping, images, timestamp, items }) => (
+							<Order
+								key={id}
+								id={id}
+								amount={amount}
+								amountShipping={amount_shipping}
+								images={images}
+								timestamp={timestamp}
+								items={items}
+							/>
+						)
+					)}
+				</div>
+			</main>
+		</div>
+	)
+}
+
+export async function getServerSideProps(context) {
+	const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+	const session = getSession(context)
+
+	if (!session) {
+		return {
+			props: {},
+		}
+	}
+
+	const stripeOrders = await stripe.orders.list({
+		customer: session.user.email,
+		limit: 100,
+	})
+
+	const orders = await Promise.all(
+		stripeOrders.data.map(async (order) => ({
+			id: order.id,
+			amount: order.amount,
+			amount_shipping: order.amount_shipping,
+			images: (
+				await stripe.files.list({
+					limit: 1,
+					type: 'order_receipt',
+					order: order.id,
+				})
+			).data[0].url,
+			timestamp: order.created,
+			items: (
+				await stripe.checkout.sessions.listLineItems(order.checkout.session, {
+					limit: 100,
+				})
+			).data,
+		}))
+	)
+}
